b84eeb757f666b6a07d65f987c6fbb44f8dcc619
Loloppe <LaurentBlaisCote@outlook.com>
2023-05-26 23:28:20 -0400

    Add project files.

---
+++ Height/HarmonyPatches/Height.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using Height.Configuration;
+using System;
+using UnityEngine;
+
+namespace PreciseHeight.HarmonyPatches
+{
+    [HarmonyPatch(typeof(PlayerHeightSettingsController), nameof(PlayerHeightSettingsController.AutoSetHeight))]
+    internal class AutoSetHeight
+    {
+        static bool Prefix(ref float ____value, ref Vector3SO ____roomCenter, ref PlayerHeightSettingsController __instance, ref Action<float> ___valueDidChangeEvent)
+        {
+            if (PluginConfig.Instance.Enabled)
+            {
+                ____value = PluginConfig.Instance.Height;
+                __instance.RefreshUI();
+                ___valueDidChangeEvent?.Invoke(____value);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(RoomAdjustSettingsViewController), nameof(RoomAdjustSettingsViewController.Move))]
+    internal class Move
+    {
+        static bool Prefix(Vector3 move, ref Vector3SO ____roomCenter, ref RoomAdjustSettingsViewController __instance)
+        {
+            if (PluginConfig.Instance.Enabled)
+            {
+                if (move.y != 0)
+                {
+                    move /= 5;
+                }
+                else
+                {
+                    move /= 10;
+                }
+
+                ____roomCenter.value += move;
+                __instance.RefreshTexts();
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(RoomAdjustSettingsViewController), nameof(RoomAdjustSettingsViewController.Rotate))]
+    internal class Rotate
+    {
+        static bool Prefix(float rotation, ref FloatSO ____roomRotation, ref RoomAdjustSettingsViewController __instance)
+        {
+            if (PluginConfig.Instance.Enabled)
+            {
+                rotation /= 5;
+                ____roomRotation.value = (rotation + ____roomRotation.value) % 360f;
+                __instance.RefreshTexts();
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Height/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BeatSaberMarkupLanguage.GameplaySetup;
+using HarmonyLib;
+using Height.Configuration;
+using IPA;
+using IPA.Config.Stores;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Height
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony harmony { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger, IPA.Config.Config conf)
+        {
+            Instance = this;
+            Log = logger;
+            PluginConfig.Instance = conf.Generated<PluginConfig>();
+            harmony = new Harmony("Loloppe.BeatSaber.Height");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            GameplaySetup.instance.AddTab("Height", "Height.Views.settings.bsml", PluginConfig.Instance, MenuType.All);
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            harmony.UnpatchSelf();
+            GameplaySetup.instance.RemoveTab("Height");
+        }
+    }
+}
